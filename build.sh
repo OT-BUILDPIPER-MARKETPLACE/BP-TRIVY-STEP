c4347a8014191c422b5d90bdf2beeeec767467a0
Sandeep <sandeep@opstree.com>
2022-10-21 12:09:04 +0530

    Updated build script to do image scanning

--- build.sh
+++ build.sh
@@ -2,3 +2,25 @@
 source functions.sh
 
 logInfoMessage "I'll do the scanning for $SCANNER"
+
+if [ -z "$IMAGE_NAME" ] || [ -z "$IMAGE_TAG" ]
+then
+    logInfoMessage "Image name/tag is not provided in env variable $IMAGE_NAME checking it in BP data"
+    logInfoMessage "Image Name -> ${IMAGE_NAME}"
+    logInfoMessage "Image Tag -> ${IMAGE_TAG}"
+    IMAGE_NAME=`getComponentName`
+    IMAGE_TAG=`getRepositoryTag`
+fi
+
+if [ -z "$IMAGE_NAME" ] || [ -z "$IMAGE_TAG" ]
+then
+    logErrorMessage "Image name/tag is not available in BP data as well please check!!!!!!"
+    logInfoMessage "Image Name -> ${IMAGE_NAME}"
+    logInfoMessage "Image Tag -> ${IMAGE_TAG}"
+    STATUS=ERROR
+else
+    logInfoMessage "I'll scan image ${IMAGE_NAME}:${IMAGE_TAG} for only ${SCAN_SEVERITY} severities"
+    sleep  $SLEEP_DURATION
+    trivy trivy image --severity ${SCAN_SEVERITY} ${IMAGE_NAME}:${IMAGE_TAG}
+    STATUS="success"
+fi
