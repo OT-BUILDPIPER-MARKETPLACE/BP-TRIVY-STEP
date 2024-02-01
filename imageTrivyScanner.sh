467559ea27e368df17e2a50b8a25a91ed47c47dd
rishops <rishabh.bohra@opstree.com>
2024-02-01 16:44:09 +0530

    feat: add MI support

--- imageTrivyScanner.sh
+++ imageTrivyScanner.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 source functions.sh
+source mi-functions.sh
+source log-functions.sh
+source file-functions.sh
+
+export base64EncodedResponse=`encodeFileContent reports/trivy-results.json`
+export application=ot-demo-ms
+export environment=`getProjectEnv`
+export service=`getServiceName`
+export organization=bp
+export source_key=trivy
+export report_file_path=null
 
 cd ${WORKSPACE}/${CODEBASE_DIR}
 
@@ -29,11 +40,15 @@ else
     logInfoMessage "I'll scan image ${IMAGE_NAME}:${IMAGE_TAG} for only ${SCAN_SEVERITY} severities"
     sleep  $SLEEP_DURATION
     logInfoMessage "Executing command"
-    logInfoMessage "trivy image -q --severity ${SCAN_SEVERITY} ${IMAGE_NAME}:${IMAGE_TAG}"
-    trivy image -q --severity ${SCAN_SEVERITY} ${IMAGE_NAME}:${IMAGE_TAG} 
-    logInfoMessage "trivy image -q --severity ${SCAN_SEVERITY} --exit-code 1 ${FORMAT_ARG} reports/${OUTPUT_ARG} ${IMAGE_NAME}:${IMAGE_TAG}"
-    trivy image -q --severity ${SCAN_SEVERITY} --exit-code 1 ${FORMAT_ARG} reports${OUTPUT_ARG} ${IMAGE_NAME}:${IMAGE_TAG} 
+    # logInfoMessage "trivy image -q --severity ${SCAN_SEVERITY} ${IMAGE_NAME}:${IMAGE_TAG}"
+    # trivy image -q --severity ${SCAN_SEVERITY} ${IMAGE_NAME}:${IMAGE_TAG} 
+    logInfoMessage "trivy image -q --severity ${SCAN_SEVERITY} --exit-code 1 ${FORMAT_ARG} ${OUTPUT_ARG} ${IMAGE_NAME}:${IMAGE_TAG}"
+    trivy image -q --severity ${SCAN_SEVERITY} --exit-code 1 ${FORMAT_ARG} ${OUTPUT_ARG} ${IMAGE_NAME}:${IMAGE_TAG} 
     STATUS=`echo $?`
+    
+    generateMIDataJson /opt/buildpiper/data/mi.template trivy.mi
+    cat trivy.mi
+    sendMIData trivy.mi http://122.160.30.218:60901
 fi
 
 if [ $STATUS -eq 0 ]
