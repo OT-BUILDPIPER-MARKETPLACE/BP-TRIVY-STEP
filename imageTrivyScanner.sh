7aa3fa476875b664d66abda88d968d3cfc76fa58
Aayush Verma <aayush.verma@opstree.com>
2026-01-04 17:04:08 +0530

    Add logging for image pull when not found locally

--- imageTrivyScanner.sh
+++ imageTrivyScanner.sh
@@ -50,10 +50,10 @@ fi
 
 if docker image inspect "${IMAGE_NAME}:${IMAGE_TAG}" >/dev/null 2>&1; then
     logInfoMessage "Image found locally: ${IMAGE_NAME}:${IMAGE_TAG}"
-else    
+else
+    logWarningMessage "Image not found locally. Pulling ${IMAGE_NAME}:${IMAGE_TAG}"
     logInfoMessage "Logging into configured registries"
     login_all_registries
-    logWarningMessage "Image not found locally. Pulling ${IMAGE_NAME}:${IMAGE_TAG}"
     docker pull "${IMAGE_NAME}:${IMAGE_TAG}"
     logInfoMessage "Image successful pull ${IMAGE_NAME}:${IMAGE_TAG}"
     if [[ $? -ne 0 ]]; then
